1c5c1e267d96de1a487ab7d7b2e67fe99492265b
0ako <andychenko@gmail.com>
2021-06-25 20:42:16 +0800

    arrays update

--- arrays.rb
+++ arrays.rb
@@ -59,8 +59,11 @@ str_array.last(2)       #=> ["small", "array"]
 ADDING AND REMOVING ELEMENTS
 Adding an element to an existing array is as simple as using the #push method or the shovel operator <<. 
 Both methods will add elements to the end of an array and return that array with the new elements. 
+Both the push and the << methods mutate the caller, so the original array is modified.
 
 The #pop method will remove the element at the end of an array and return the element that was removed.
+Note that when we called the pop method, the original array variable was modified (ie, this is also a method that mutates the caller), 
+but the returned value is the popped element, as shown in the first line above. 
 
 num_array = [1, 2]
 num_array.push(3, 4)      #=> [1, 2, 3, 4]
@@ -127,9 +130,146 @@ Here is a brief look at some other common array methods you might run into:
 
 [1, 2, 3].reverse       #=> [3, 2, 1]
 
+The flatten method can be used to take an array that contains nested arrays and create a one-dimensional array.
+   
+    irb: 001 > a = [1, 2, [3, 4, 5], [6, 7]]
+    => [1, 2, [3, 4, 5], [6, 7]]
+    irb: 002 > a.flatten
+    => [1, 2, 3, 4, 5, 6, 7]
+
+    The flatten method is not destructive. 
+
+The include? method checks to see if the argument given is included in the array. 
+It has a question mark at the end of it which usually means that you should expect it to return a boolean value, true or false. 
+(Such methods are called predicates.) 
+Just like the methods that end in a "!", this is strictly by convention only and not a property of the language.
 [1, 2, 3].include?(3)   #=> true
 [1, 2, 3].include?("3") #=> false
 
 [1, 2, 3].join          #=> "123"
 [1, 2, 3].join("-")     #=> "1-2-3"
+
+https://launchschool.com/books/ruby/read/arrays
+
+MODIFYING ARRAYS________________________________________________________________________________________________________________________________
+Often you'll have an array and you'll want to operate on many of the elements in the array the same way. 
+Ruby has many methods that do these types of operations.
+
+The map method iterates over an array applying a block to each element of the array and returns a new array with those results. 
+The irb session below shows how to use map to get the square of all numbers in an array. 
+The collect method is an alias to map - they do the same thing.
+
+    irb :001 > a = [1, 2, 3, 4]
+    => [1, 2, 3, 4]
+    irb :002 > a.map { |num| num**2 }
+    => [1, 4, 9, 16]
+    irb :003 > a.collect { |num| num**2 }
+    => [1, 4, 9, 16]
+    irb :004 > a
+    => [1, 2, 3, 4]
+
+# You'll notice that after performing these methods there is no change to the initial array. 
+# These methods are not destructive (i.e., they don't mutate the caller). How do you know which methods mutate the caller and which ones don't? 
+# You have to use the methods and pay attention to the output in irb; that is, you have to memorize or know through using it.
+
+# The delete_at method can be helpful if you'd like to eliminate the value at a certain index from your array. 
+# This modifies your array destructively. Once you call this method, you are changing your array permanently.
+
+    irb :005 > a.delete_at(1)
+    => 2
+    irb :006 > a
+    => [1, 3, 4]
+
+Sometimes you will know the value that you want to delete, but not the index. In these situations you will want to use the delete method. 
+The delete method permanently deletes all instances of the provided value from the array.
+
+    irb :007 > my_pets = ["cat", "dog", "bird", "cat", "snake"]
+    => ["cat", "dog", "bird", "cat", "snake"]
+    irb :008 > my_pets.delete("cat")
+    => "cat"
+    irb :009 > my_pets
+    => ["dog", "bird", "snake"]
+
+The uniq method iterates through an array, deletes any duplicate values that exist, then returns the result as a new array.
+The uniq method does not modify the original b array; it returns a new array with the duplicates removed.
+
+    irb :010 > b = [1, 1, 2, 2, 3, 3, 4, 4]
+    => [1, 1, 2, 2, 3, 3, 4, 4]
+    irb :011 > b.uniq
+    => [1, 2, 3, 4]
+    irb :012 > b
+    => [1, 1, 2, 2, 3, 3, 4, 4]
+
+If you add the bang suffix (!) to this method, you can perform the uniq function destructively. Much like the way the delete method works.
+
+    irb :013 > b = [1, 1, 2, 2, 3, 3, 4, 4]
+    => [1, 1, 2, 2, 3, 3, 4, 4]
+    irb :014 > b.uniq!
+    => [1, 2, 3, 4]
+    irb :015 > b
+    => [1, 2, 3, 4]
+
+uniq and uniq! are two different methods for Ruby Arrays. You cannot simply append a ! onto any method and achieve a destructive operation.
+
+ITERATING OVER AN ARRAY_____________________________________________________________________________________________________________________
+# We talked in the loop section about using each to iterate over an array. The Ruby standard library has many similar methods. 
+# Let's take a look at the select method. 
+# This method iterates over an array and returns a new array that includes any items that return true to the expression provided. 
+# That's a mouthful. As always, looking at code is more helpful than using a bunch of words.
+
+    irb :001 > numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    irb :002 > numbers.select { |number| number > 4 }
+    => [5, 6, 7, 8, 9, 10]
+    irb :003 > numbers
+    => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+The select method selects all of the numbers that are greater than 4 and returns them in an array. 
+It does not mutate the caller (the original numbers array is unmodified).
+
+Methods With a !
+The bang suffix (!) at the end of the method name usually indicates that the method will change (or mutate) the caller permanently. 
+Unfortunately this is not always the case. 
+It is a good rule to be wary of any method that has the bang suffix and to make sure to check the Ruby documentation 
+to see if it will behave destructively (the word "destructive" here just means mutating the caller).
+
+Also, please note that there are methods like pop and push that are destructive, but do not have a ! at the end. 
+
+Mutating the Caller: The Sequel
+https://launchschool.com/books/ruby/read/arrays#mutatingthecallerthesequel
+
+Nested Arrays
+https://launchschool.com/books/ruby/read/arrays#nestedarrays
+
+Comparing Arrays
+https://launchschool.com/books/ruby/read/arrays#comparingarrays 
+You can compare arrays for equality using the == operator.
+
+    irb :001 > a = [1, 2, 3]
+    => [1, 2, 3]
+    irb :002 > b = [2, 3, 4]
+    => [2, 3, 4]
+    irb :003 > a == b
+    => false
+    irb :004 > b.pop
+    => 4
+    irb :005 > b.unshift(1)
+    => [1, 2, 3]
+    irb :006 > a == b
+    => true
+
+# You'll notice that we used the unshift method in this example. unshift is a lot like the push method. 
+# However, instead of adding values to the end of the list, unshift adds values to the start of the list.
+
+to_s
+https://launchschool.com/books/ruby/read/arrays#to_s
+The to_s method is used to create a string representation of an array. 
+Ruby does this behind the scenes when you use string interpolation to print an array to the screen.
+
+    irb :001 > a = [1, 2, 3]
+    => [1, 2, 3]
+    irb :002 > "It's as easy as #{a}"
+    => "It's as easy as [1, 2, 3]"
+
+In order to get our array to print properly, Ruby is calling the to_s method on our array and adding it into the string.
 
